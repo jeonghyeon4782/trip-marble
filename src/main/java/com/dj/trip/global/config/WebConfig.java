897f662aa1438c194dc724ef7d9dc5d288cec9f1
ensk26 <tjensk26@gmail.com>
2024-05-02 03:33:51 +0900

    feat: OauthserverType의 converter 추가, cors 허용 경로 추가

--- src/main/java/com/dj/trip/global/config/WebConfig.java
+++ src/main/java/com/dj/trip/global/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.dj.trip.global.config;
 
+import com.dj.trip.domain.oauth.OauthServerTypeConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:8080")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
@@ -23,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowCredentials(true)
                 .exposedHeaders("*");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OauthServerTypeConverter());
     }
 }
