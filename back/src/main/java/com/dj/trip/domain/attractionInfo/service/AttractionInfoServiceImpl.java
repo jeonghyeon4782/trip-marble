d431acfe5047b1515432add62b021056231b6a9a
wjd47 <wjd4782@naver.com>
2024-05-17 14:40:02 +0900

    feat: AttractionInfo 관련 api 구현

---
+++ back/src/main/java/com/dj/trip/domain/attractionInfo/service/AttractionInfoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.dj.trip.domain.attractionInfo.service;
+
+import com.dj.trip.domain.attractionInfo.dto.GetAttarctionInfoResponseDto;
+import com.dj.trip.domain.attractionInfo.mapper.AttractionInfoMapper;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AttractionInfoServiceImpl implements AttractionInfoService {
+
+    private final AttractionInfoMapper attractionInfoMapper;
+    private final ModelMapper modelMapper;
+
+    // memberBoardMapId로 현재 캐릭터 위치에 있는 관광지 정보 조회
+    @Override
+    public GetAttarctionInfoResponseDto getAttractionInfoNowLocation(int memberBoardMapId) {
+        return modelMapper.map(attractionInfoMapper.selectAttractionInfoByMemberBoardMapId(memberBoardMapId), GetAttarctionInfoResponseDto.class);
+    }
+}
