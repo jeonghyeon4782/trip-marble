cb385eececcc96e369d33a5a3e966916d08309e1
ensk26 <tjensk26@gmail.com>
2024-05-17 13:44:21 +0900

    feat: 소셜 로그인 리다이렉트 화면

---
+++ front/src/views/TheOauthView.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { oauthMember } from '@/api/auth';
+import { ref } from 'vue';
+import { useRoute, useRouter } from "vue-router";
+
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get('code');
+console.log('인증코드: ', code);
+
+const route = useRoute();
+const router = useRouter();
+
+const { oauth_server_type } = route.params;
+
+const requestData = ref({
+    code: code,
+    oauthServerType: oauth_server_type
+});
+
+oauthMember(
+    requestData.value,
+    (response) => {
+        if (response.status == 200) {
+            console.log(response.data.data.isFirst);
+            if (response.data.data.isFirst) {
+                router.replace({
+                    name: 'signup',
+                    query: {
+                        email: response.data.data.email,
+                        oauthServerType: response.data.data.oauthServerType
+                    }
+                });
+            } else {
+                router.replace({ name: 'main' });
+            }
+        }
+    },
+    (error) =>{
+        console.log(error)
+    }
+)
+</script>
+
+<template>
+    <div>
+        리다이렉트
+    </div>
+</template>
+
+<style scoped></style>
