1c5d5c83af7ea989f514cdff158e5be7b7656d35
ensk26 <tjensk26@gmail.com>
2024-05-17 13:36:25 +0900

    chore: api 연결 설정

--- front/src/api/review.js
+++ front/src/api/review.js
@@ -3,28 +3,28 @@ import { localAxios } from "@/util/http-commons";
 const local = localAxios();
 
 function listReview(param, success, fail) {
-  local.get(`/review`, { params: param }).then(success).catch(fail);
+  local.get(`api/review`, { params: param }).then(success).catch(fail);
 }
 
 function detailReview(reviewid, success, fail) {
-  local.get(`/review/${reviewid}`).then(success).catch(fail);
+  local.get(`api/review/${reviewid}`).then(success).catch(fail);
 }
 
 function registReview(review, success, fail) {
   console.log("reviewjs review", review);
-  local.post(`/review`, JSON.stringify(reivew)).then(success).catch(fail);
+  local.post(`api/review`, JSON.stringify(reivew)).then(success).catch(fail);
 }
 
 function getModifyReview(reviewid, success, fail) {
-  local.get(`/review/modify/${reviewid}`).then(success).catch(fail);
+  local.get(`api/review/modify/${reviewid}`).then(success).catch(fail);
 }
 
 function modifyReview(review, success, fail) {
-  local.put(`/review`, JSON.stringify(review)).then(success).catch(fail);
+  local.put(`api/review`, JSON.stringify(review)).then(success).catch(fail);
 }
 
 function deleteReview(reviewid, success, fail) {
-  local.delete(`/review/${reviewid}`).then(success).catch(fail);
+  local.delete(`api/review/${reviewid}`).then(success).catch(fail);
 }
 
 export {
--- front/src/util/http-commons.js
+++ front/src/util/http-commons.js
@@ -5,11 +5,11 @@ const { VITE_VUE_API_URL, VITE_POSTMAN_TEST_API_URL } = import.meta.env;
 // local vue api axios instance
 function localAxios() {
   const instance = axios.create({
-    // mock api로 test 중
-    baseURL: VITE_POSTMAN_TEST_API_URL,
+    baseURL: VITE_VUE_API_URL,
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
+      "Content-Type": "application/json;charset=utf-8"
     },
+    withCredentials: true
   });
   return instance;
 }
