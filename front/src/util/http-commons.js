eed82ec416b3410f57117a4cfcb4d38f9c6e9662
ensk26 <tjensk26@gmail.com>
2024-05-18 16:12:39 +0900

    feat: 토큰 상태 관리

--- front/src/util/http-commons.js
+++ front/src/util/http-commons.js
@@ -1,6 +1,9 @@
+import router from "@/router";
 import axios from "axios";
 
 const { VITE_VUE_API_URL, VITE_POSTMAN_TEST_API_URL } = import.meta.env;
+
+axios.defaults.withCredentials = true;
 
 // local vue api axios instance
 function localAxios() {
@@ -8,9 +11,32 @@ function localAxios() {
     baseURL: VITE_VUE_API_URL,
     headers: {
       "Content-Type": "application/json;charset=utf-8"
-    },
-    withCredentials: true
+    }
   });
+
+  instance.interceptors.response.use(
+    response => {
+      return response;
+    },
+    async error => {
+      const originalRequest = error.config;
+      if (error.response && error.response.status === 403 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          const response = await axios.post(VITE_VUE_API_URL + '/auth/refreshToken');
+          console.log("엑세스 토큰 재 발급" + response.data);
+          return axios(originalRequest);
+        } catch (refreshTokenError) {
+          console.log(error.response.data.msg)
+          let msg = "로그인 필요";
+          alert(msg)
+          router.replace({ name: "login" });
+          return Promise.reject(refreshTokenError);
+        }
+      }
+      return Promise.reject(error);
+    }
+  )
   return instance;
 }
 
